59ec7fa5b2cdadc599cc20eed618be7c14c4067e
bruninhogomez <gcbruno@iesrafaelalberti.es>
2024-10-21 19:53:26 +0200

    Commit 2 . ejercicio 21

--- src/ej21_01.py
+++ src/ej21_01.py
@@ -1,1 +1,24 @@
+"Este es el ejercicio 21"
+def pedir_pass() -> str:
+    return input("Introduce la contraseña: ").lower()
 
+def comprobar_pass(pass_usuario, pass_secreta)->bool:
+
+    if pass_usuario == pass_secreta:
+        return True
+    else:
+        return False
+
+
+def main ():
+    pass_secreta = "contraseña"
+
+    pass_usuario = pedir_pass()
+
+    if comprobar_pass(pass_usuario , pass_secreta)==True:
+        print("Contraseña correcta")
+    else:
+        print("ERROR* contraseña incorrecta")
+        
+if __name__=="__main__":
+    main()
